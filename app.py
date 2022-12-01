e355e13009f8015ee2b72750c22064ef938aa16d
Alamka <miliwan@yandex.ru>
2022-12-01 21:32:52 +0300

    Добавлена фамилия автору

--- app.py
+++ app.py
@@ -21,10 +21,11 @@ migrate = Migrate(app, db)
 class AuthorModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=True)
+    surname = db.Column(db.String(64))
     quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic', cascade="all, delete-orphan")
 
-    def __init__(self, name):
-        self.name = name
+    #def __init__(self, name):
+    #    self.name = name
 
     def to_dict(self):
         return {
